4e5441617b48d7261b4c1a61f8e6969c2e4fe6ce
Georg Westner <Georg Westner>
2017-05-25 15:46:39 +0200

    customer updated

--- app/controllers/CustomersController.java
+++ app/controllers/CustomersController.java
@@ -1,12 +1,16 @@
 package controllers;
 
+import models.Customer;
 import play.mvc.*;
+
+import java.util.List;
 
 /**
  * Created by georg on 21.05.2017.
  */
 public class CustomersController extends Controller{
     public Result index() {
-        return ok(views.html.customers.render());
+        List<Customer> customerList = Customer.find.all();
+        return ok(views.html.customers.render(customerList));
     }
 }
--- target/scala-2.11/twirl/main/views/html/customers.template.scala
+++ target/scala-2.11/twirl/main/views/html/customers.template.scala
@@ -21,24 +21,52 @@ import play.data._
 import play.api.data.Field
 import play.mvc.Http.Context.Implicit._
 
-class customers extends BaseScalaTemplate[play.twirl.api.HtmlFormat.Appendable,Format[play.twirl.api.HtmlFormat.Appendable]](play.twirl.api.HtmlFormat) with play.twirl.api.Template0[play.twirl.api.HtmlFormat.Appendable] {
+class customers extends BaseScalaTemplate[play.twirl.api.HtmlFormat.Appendable,Format[play.twirl.api.HtmlFormat.Appendable]](play.twirl.api.HtmlFormat) with play.twirl.api.Template1[List[Customer],play.twirl.api.HtmlFormat.Appendable] {
 
   /**/
-  def apply():play.twirl.api.HtmlFormat.Appendable = {
+  def apply/*1.2*/(customerList: List[Customer]):play.twirl.api.HtmlFormat.Appendable = {
     _display_ {
       {
 
 
-Seq[Any](_display_(/*1.2*/main("Customers")/*1.19*/{_display_(Seq[Any](format.raw/*1.20*/("""
-    """),format.raw/*2.5*/("""<h1>Customers</h1>
+Seq[Any](format.raw/*1.32*/("""
+
+"""),_display_(/*3.2*/main("Customer")/*3.18*/{_display_(Seq[Any](format.raw/*3.19*/("""
+    """),format.raw/*4.5*/("""<h1>Customer</h1>
+    """),_display_(/*5.6*/if(customerList.size() > 0)/*5.33*/ {_display_(Seq[Any](format.raw/*5.35*/("""
+        """),format.raw/*6.9*/("""<div class="row">
+            <div class="col-xs-12">
+                <table class="table table-hover">
+                    <thead>
+                        <tr>
+                            <td>Name</td>
+                        </tr>
+                    </thead>
+                    <tbody>
+                    """),_display_(/*15.22*/for(customer <- customerList) yield /*15.51*/{_display_(Seq[Any](format.raw/*15.52*/("""
+                        """),format.raw/*16.25*/("""<tr>
+                            <td>"""),_display_(/*17.34*/customer/*17.42*/.getPrename()),format.raw/*17.55*/("""</td>
+                        </tr>
+
+                    """)))}),format.raw/*20.22*/("""
+                    """),format.raw/*21.21*/("""</tbody>
+                </table>
+            </div>
+
+        </div>
+    """)))}/*26.7*/else/*26.12*/{_display_(Seq[Any](format.raw/*26.13*/("""
+        """),format.raw/*27.9*/("""<div class="project-item">
+            Kein Project vorhanden
+        </div>
+    """)))}),format.raw/*30.6*/("""
 """)))}))
       }
     }
   }
 
-  def render(): play.twirl.api.HtmlFormat.Appendable = apply()
+  def render(customerList:List[Customer]): play.twirl.api.HtmlFormat.Appendable = apply(customerList)
 
-  def f:(() => play.twirl.api.HtmlFormat.Appendable) = () => apply()
+  def f:((List[Customer]) => play.twirl.api.HtmlFormat.Appendable) = (customerList) => apply(customerList)
 
   def ref: this.type = this
 
@@ -51,11 +79,11 @@ Seq[Any](_display_(/*1.2*/main("Customers")/*1.19*/{_display_(Seq[Any](format.ra
 object customers extends customers_Scope0.customers
               /*
                   -- GENERATED --
-                  DATE: Sun May 21 19:07:48 CEST 2017
+                  DATE: Thu May 25 15:46:04 CEST 2017
                   SOURCE: C:/Users/georg/Documents/projects/otten/otten/app/views/customers.scala.html
-                  HASH: c5575bd4ac3a608259445bf322ebfb1dba684c71
-                  MATRIX: 835->1|860->18|898->19|930->25
-                  LINES: 32->1|32->1|32->1|33->2
+                  HASH: 2aab5a8a7a90b7911065a6eb7f62912caf32296c
+                  MATRIX: 761->1|886->31|916->36|940->52|978->53|1010->59|1059->83|1094->110|1133->112|1169->122|1516->442|1561->471|1600->472|1654->498|1720->537|1737->545|1771->558|1863->619|1913->641|2010->721|2023->726|2062->727|2099->737|2214->822
+                  LINES: 27->1|32->1|34->3|34->3|34->3|35->4|36->5|36->5|36->5|37->6|46->15|46->15|46->15|47->16|48->17|48->17|48->17|51->20|52->21|57->26|57->26|57->26|58->27|61->30
                   -- GENERATED --
               */
           
