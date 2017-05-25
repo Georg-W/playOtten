be04d0f04af99de18403645b2f48f548d5ae0d5d
Georg Westner <Georg Westner>
2017-05-25 12:33:39 +0200

    updated sql

--- conf/evolutions/default/1.sql
+++ conf/evolutions/default/1.sql
@@ -51,39 +51,40 @@ create table user (
 alter table task add constraint fk_task_project foreign key (task_id) references project (project_id) on delete restrict on update restrict;
 create index ix_task_project on task (task_id);
 
-alter table product_tag add constraint fk_product_tag_tag foreign key (tag_tag_id) references tag (tag_id) on delete restrict on update restrict;
-create index ix_product_tag_tag on product_tag (tag_tag_id);
+alter table task add constraint fk_task_worker foreign key (task_id) references worker (worker_id) on delete restrict on update restrict;
+create index ix_task_worker on task (task_id);
 
-alter table stock_item add constraint fk_stock_item_warehouse_warehouse_id foreign key (warehouse_warehouse_id) references warehouse (warehouse_id) on delete restrict on update restrict;
-create index ix_stock_item_warehouse_warehouse_id on stock_item (warehouse_warehouse_id);
+alter table project add constraint fk_project_customer foreign key (project_id) references customer (customer_id) on delete restrict on update restrict;
+create index ix_project_customer on project (project_id);
 
-alter table stock_item add constraint fk_stock_item_product_product_id foreign key (product_product_id) references product (product_id) on delete restrict on update restrict;
-create index ix_stock_item_product_product_id on stock_item (product_product_id);
+alter table otten add constraint fk_otten_task foreign key (otten_id) references task (task_id) on delete restrict on update restrict;
+create index ix_otten_task_id on otten (otten_id);
+
 
 
 # --- !Downs
 
-alter table product_tag drop foreign key fk_product_tag_product;
-drop index ix_product_tag_product on product_tag;
+alter table task drop foreign key fk_task_project;
+drop index ix_task_project on task;
 
-alter table product_tag drop foreign key fk_product_tag_tag;
-drop index ix_product_tag_tag on product_tag;
+alter table task drop foreign key fk_task_worker;
+drop index ix_task_worker on task;
 
-alter table stock_item drop foreign key fk_stock_item_warehouse_warehouse_id;
-drop index ix_stock_item_warehouse_warehouse_id on stock_item;
+alter table project drop foreign key fk_project_customer;
+drop index ix_project_customer on project;
 
-alter table stock_item drop foreign key fk_stock_item_product_product_id;
-drop index ix_stock_item_product_product_id on stock_item;
+alter table otten drop foreign key fk_otten_task;
+drop index ix_otten_task on otten;
 
-drop table if exists product;
-
-drop table if exists product_tag;
-
-drop table if exists stock_item;
-
-drop table if exists tag;
+drop table if exists project;
 
 drop table if exists user;
 
-drop table if exists warehouse;
+drop table if exists otten;
+
+drop table if exists worker;
+
+drop table if exists customer;
+
+drop table if exists task;
 
